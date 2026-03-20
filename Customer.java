337fcf06b1f7a87f62cc223c87b5b507a4152d2e
tommym-r <tommymro@gmail.com>
2026-03-20 20:33:23 +0000

    finished user, started on customer

---
+++ Customer.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.*;
+
+public class Customer extends User {
+  private CheckingAccount checking;
+  private SavingsAccount savings;
+
+  public static void main(String args []){
+    Customer c = new Customer();
+    c.setUserName("Alice");
+    c.setPIN("1111");
+    c.start();
+  } // end main
+
+  public Customer(){
+    checking = new CheckingAccount();
+    savings = new SavingsAccount();
+  } // end constructor
+
+  public String menu(){
+    Scanner input = new Scanner(System.in);
+    System.out.println();
+    System.out.println("0) exit");
+    System.out.println("1) Manage Checking Account");
+    System.out.println("2) Manage Savings Account");
+    System.out.println("3) Change PIN");
+    System.out.println();
+    System.out.println("Please enter 0-3");
+    String response = input.nextLine();
+    return response;
+  } // end menu
+
+  public void start(){
+    boolean keepGoing = true;
+    String response;
+    boolean login = true;
+    while (keepGoing){
+      login = login();
+      if (login == true){
+        System.out.println("Login Successful");
+        response = menu();
+        if (response.equals("0")){
+          keepGoing = false;
+        } else if (response.equals("1")){
+          checking.start();
+        } else if (response.equals("2")){
+          savings.start();
+        } else if (response.equals("3")){
+          this.changePIN();
+        } else {
+          System.out.println("Please enter 0, 1, 2, or 3");
+        } // end if
+      } else {
+        System.out.println("Incorrect username or password, try again");
+    } // end while
+  } // end start
+
+  public void changePIN(){
+    boolean keepGoing = true;
+    Scanner input = new Scanner(System.in);
+    String response;
+    String tempPIN;
+    
+    while (keepGoing){
+      System.out.println("Enter your current PIN: ");
+      response = input.nextLine();
+      if (response.equals(this.PIN)){
+        System.out.println();
+        System.out.println("Enter your new PIN: ");
+        tempPIN = input.nextLine;
+        System.out.println();
+        System.out.println("Re-enter your new PIN: ");
+        response = input.nextLine();
+        if (response.equals(tempPIN)){
+          this.PIN = tempPIN;
+          System.out.println("PIN changed successfully");
+          keepGoing = false;
+        } else {
+          System.out.println("Your PINs did not match, please try again");
+        } // end if
+      } else {
+        System.out.println("Incorrect PIN, please try again");
+      } // end if
+    } // end while
+  } // end changePIN
+  
+
+  public String getReport(){
+    System.out.println("User Report: ");
+    System.out.println("Checking balance: " + checking.getBalanceString());
+    System.out.println("Savings balance: " + savings.getBalanceString());
+  } // end getReport
+
+} // end Customer
---
+++ User.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public abstract class User implements HasMenu, Serializable{
+  private String userName;
+  private String PIN;
+
+  public boolean login(){
+    boolean success = false;
+    Scanner input = new Scanner(System.in);
+    System.out.println("User name: ");
+    String userName = input.nextLine();
+    System.out.println("PIN: ");
+    String PIN = input.nextLine();
+    if (userName.equals(this.userName)){
+      if (PIN.equals(this.PIN)){
+        success = true;
+      } // end if
+    } // end if
+    return success;
+  } // end login
+  
+  public boolean login(String userName, String PIN){
+    boolean success = false;
+    if (userName.equals(this.userName)){
+      if (PIN.equals(this.PIN)){
+        success = true;
+      } // end if
+    } // end if
+    return success;
+  } // end login
+
+  public void setUserName(String userName){
+    this.userName = userName;
+  } // end setUserName
+
+  public String getUserName(){
+    return this.userName;
+  } // end getUserName
+
+  public void setPIN(String PIN){
+    this.PIN = PIN;
+  } // end setPIN
+
+  public String getPIN(){
+    return this.PIN;
+  } // end getPIN
+
+  public abstract String getReport();
+} // end User
